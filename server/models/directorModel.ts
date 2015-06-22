f83ca7b1f147de7fd6e667e5641aff3bf002a1d4
Ilya Potuzhnov <ipotuzhnov@gmail.com>
2015-06-22 19:30:31 +0300

    Clean up

--- server/models/directorModel.ts
+++ server/models/directorModel.ts
@@ -2,44 +2,6 @@
 
 import db = require('../db');
 
-/*
-class Director implements IDirector {
-  private _obj: IDirector;
-  
-  constructor(obj: IDirector) {
-    this._obj = obj;
-  }
-  
-  get livestream_id() {
-    return this._obj.livestream_id;
-  }
-  
-  get full_name() {
-    return this._obj.full_name;
-  }
-  
-  get dob() {
-    return this._obj.dob;
-  }
-  
-  get favorite_camera() {
-    return this._obj.favorite_camera;
-  }
-  
-  set favorite_camera(val: string) {
-    this._obj.favorite_camera = val;
-  }
-  
-  get favorite_movies() {
-    return this._obj.favorite_movies;
-  }
-  
-  set favorite_movies(val: string[]) {
-    this._obj.favorite_movies = val;
-  }
-}
-*/
-
 var directorModel = db.model('directors');
 
 export = directorModel;
