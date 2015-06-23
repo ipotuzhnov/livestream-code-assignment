c01d495362ad50670c4cb67dc8de54d90dc32b49
Ilya Potuzhnov <ipotuzhnov@gmail.com>
2015-06-23 04:51:48 +0300

    Clean up

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Directors API with Node.js
-----
 A simple API that allows to register and list all the movie directors having an
 account on Livestream.
 
--- server/app.ts
+++ server/app.ts
@@ -13,23 +13,18 @@ import db = require('./db');
 import Director = require('./models/directorModel');
 
 import routes = require('./routes/index');
-//import users = require('./routes/users');
 import directorRoutes = require('./routes/directorRoutes');
 var directorRouter = directorRoutes(Director);
 
 var app = express();
-//var db = redis.createClient();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
